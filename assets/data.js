ff358da65ed0d25d16bfe8b8026e4ffbf64c28c0
harshrai654 <harshraitth2@gmail.com>
2021-01-19 23:46:36 +0530

    Question answer field updated

--- assets/data.js
+++ assets/data.js
@@ -2,28 +2,28 @@ const data = {
     questions:[
         {
             text:"dummy question1",
-            opts:["A","B","C","D"],
-            ans:"B"
+            opts:["A1","B1","C1","D1"],
+            ans:0
         },
         {
             text:"dummy question2",
-            opts:["A","B","C","D"],
-            ans:"B"
+            opts:["A2","B2","C2","D2"],
+            ans:1
         },
         {
             text:"dummy question3",
-            opts:["A","B","C","D"],
-            ans:"B"
+            opts:["A3","B3","C3","D3"],
+            ans:2
         },
         {
             text:"dummy question4",
-            opts:["A","B","C","D"],
-            ans:"B"
+            opts:["A4","B4","C4","D4"],
+            ans:3
         },
         {
             text:"dummy question5",
-            opts:["A","B","C","D"],
-            ans:"B"
+            opts:["A5","B5","C5","D5"],
+            ans:0
         }
     ]
 }
