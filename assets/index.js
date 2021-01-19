3ac6ea82df1b87a3762ede57ffb4f5710269cc89
harshrai654 <harshraitth2@gmail.com>
2021-01-19 22:18:56 +0530

    Simple question traversing added

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,32 @@
-console.log("Quiz app");
+import data from "./data.js";
+
+
+const init = function(){
+    let index = 0;
+    const quesArea = document.getElementById("ques");
+    const nextButton = document.getElementById("next");
+    const prevButton = document.getElementById("prev");
+    const questionList = data.questions;
+    const total = questionList.length;
+
+    quesArea.innerHTML = questionList[index].text;
+
+    nextButton.addEventListener("click", ()=>{
+        index = (index + 1)%total;
+        console.log(index)
+        document.getElementById("ques").innerHTML = questionList[index].text;
+    })
+    
+    prevButton.addEventListener("click", ()=>{
+        index = index-1 < 0?total-1:index-1;
+        console.log(index)
+        document.getElementById("ques").innerHTML = questionList[index].text;
+    })
+}
+
+init();
+
+
+
+
+
