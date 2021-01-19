f0583c03acc8a6f08110fc97a8b30428132b26af
harshrai654 <harshraitth2@gmail.com>
2021-01-19 22:17:41 +0530

    Added static data store

---
+++ assets/data.js
@@ -0,0 +1,31 @@
+const data = {
+    questions:[
+        {
+            text:"dummy question1",
+            opts:["A","B","C","D"],
+            ans:"B"
+        },
+        {
+            text:"dummy question2",
+            opts:["A","B","C","D"],
+            ans:"B"
+        },
+        {
+            text:"dummy question3",
+            opts:["A","B","C","D"],
+            ans:"B"
+        },
+        {
+            text:"dummy question4",
+            opts:["A","B","C","D"],
+            ans:"B"
+        },
+        {
+            text:"dummy question5",
+            opts:["A","B","C","D"],
+            ans:"B"
+        }
+    ]
+}
+
+export default data;
